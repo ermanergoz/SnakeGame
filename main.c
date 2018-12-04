40c692d1baebb413504898a5092aaa6207312433
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2018-12-04 12:21:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include<time.h>
 #include <locale.h>
 
-//  Created by Yusuf Erman ERGÖZ on November 2015.
+//  Created by Yusuf Erman ERGÃ–Z on November 2015.
 //  All rights reserved.
 
 struct snakes
@@ -17,7 +17,6 @@ int score=0;
 int col=20,row=10;
 char map[20][40];
 
-
 int done_or_continue(struct snakes snake[])
 {
     int i,k1=0,k2=0;
@@ -27,13 +26,10 @@ int done_or_continue(struct snakes snake[])
     }
     for(i=1; i<len; i++)
     {
-
         if(snake[0].x==snake[i].x&&snake[0].y==snake[i].y)
         {
-
             k2=1;
             break;
-
         }
     }
     if(k1==1||k2==1) return 1;
@@ -42,25 +38,20 @@ int done_or_continue(struct snakes snake[])
 
 void feeding()
 {
-
     srand(time(NULL));
     row=rand()%18+1;
     col=rand()%38+1;
-
 }
 void food(char dizi[20][40],struct snakes snake[])
 {
     if(snake[len-1].x==row&&snake[len-1].y==col)
     {
-
         snake[len].x=row;
         snake[len].y=col;
         len++;
         score=score+10;
         feeding();
         printf("\a");
-
-
     }
 }
 void wall(char dizi[20][40])
@@ -80,22 +71,15 @@ void wall(char dizi[20][40])
             dizi[i][0]='|';
             for(j=1; j<39; j++)
             {
-
                 dizi[i][j]=' ';
-
             }
-
             dizi[i][39]='|';
         }
-
     }
-
     map[row][col]='x';
-
 }
 void draw(char dizi[20][40])
 {
-
     system("CLS");
     printf("Score: %d\n",score);
 
@@ -104,14 +88,10 @@ void draw(char dizi[20][40])
     {
         for(j=0; j<40; j++)
         {
-
             printf("%c",dizi[i][j]);
-
         }
-
         printf("\n");
     }
-
 }
 void start(struct snakes age[])
 {
@@ -131,9 +111,7 @@ void snakes (char dizi[20][40],struct snakes age[])
 
     for(i=0; i<len; i++)
     {
-
         dizi[age[i].x][age[i].y]='*';
-
     }
 }
 void rightt(struct snakes snake[],int lenght)
@@ -141,12 +119,9 @@ void rightt(struct snakes snake[],int lenght)
     int i;
     for(i=lenght-1; i>0; i--)
     {
-
         snake[i].x=snake[i-1].x;
         snake[i].y=snake[i-1].y;
-
     }
-
     snake[0].y++;
 }
 void leftt(struct snakes snake[],int lenght)
@@ -154,12 +129,9 @@ void leftt(struct snakes snake[],int lenght)
     int i;
     for(i=lenght-1; i>0; i--)
     {
-
         snake[i].x=snake[i-1].x;
         snake[i].y=snake[i-1].y;
-
     }
-
     snake[0].y--;
 }
 void upp(struct snakes snake[],int lenght)
@@ -167,12 +139,9 @@ void upp(struct snakes snake[],int lenght)
     int i;
     for(i=lenght-1; i>0; i--)
     {
-
         snake[i].x=snake[i-1].x;
         snake[i].y=snake[i-1].y;
-
     }
-
     snake[0].x--;
 }
 void downn(struct snakes snake[],int lenght)
@@ -180,35 +149,24 @@ void downn(struct snakes snake[],int lenght)
     int i;
     for(i=lenght-1; i>0; i--)
     {
-
         snake[i].x=snake[i-1].x;
         snake[i].y=snake[i-1].y;
-
     }
-
     snake[0].x++;
-
 }
-
-
-
 
 int main()
 {
-
-
     printf("\n\n\t\t\t  WELCOME TO SNAKE GAME!\n");
     printf("\n\n\n\n\n\n");
     printf("\t\tUse w,a,s,d keys for direction of the snake");
     printf("\n\n\n\n\n\n");
 
     //setlocale(LC_ALL,"Turkish");
-
-    //printf(" Yusuf Erman ERGÖZ\n\n");
+    //printf(" Yusuf Erman ERGÃ–Z\n\n");
 
     system("pause");
     system("cls");
-
 
     char right='d';
     char left='a';
@@ -219,50 +177,36 @@ int main()
     int kontrol=0;
     struct snakes snake[100];
 
-
-
-
     wall(map);
     start(snake);
     snakes(map,snake);
     draw(map);
-
-
-
-
+    
     while(1)
     {
-
         direction=getch();
         i=0;
 
         if(direction==right)
         {
-
             while(i<100000)
             {
                 if(i%2000==0)
                 {
                     if(done_or_continue(snake))
                     {
-
                         draw(map);
                         kontrol=1;
                         break;
-
                     }
-
                     rightt(snake,len);
                     wall(map);
                     snakes(map,snake);
                     draw(map);
                 }
-
                 i++;
                 if(kbhit()) break;
-
             }
-
         }
         else if(direction==left)
         {
@@ -272,18 +216,14 @@ int main()
                 {
                     if(done_or_continue(snake))
                     {
-
                         draw(map);
                         kontrol=1;
-
                         break;
-
                     }
                     leftt(snake,len);
                     wall(map);
                     snakes(map,snake);
                     draw(map);
-
                 }
                 i++;
                 if(kbhit()) break;
@@ -297,22 +237,17 @@ int main()
                 {
                     if(done_or_continue(snake))
                     {
-
                         draw(map);
                         kontrol=1;
                         break;
-
-
                     }
-
                     upp(snake,len);
                     wall(map);
                     snakes(map,snake);
                     draw(map);
                 }
-
                 i++;
-
+                
                 if(kbhit()) break;
             }
         }
@@ -324,31 +259,22 @@ int main()
                 {
                     if(done_or_continue(snake))
                     {
-
                         kontrol=1;
                         draw(map);
                         kontrol=1;
                         break;
-
                     }
-
                     downn(snake,len);
                     wall(map);
                     snakes(map,snake);
                     draw(map);
-
                 }
                 i++;
 
                 if(kbhit()) break;
             }
         }
-        if(kontrol==1) break;
-
-
+        if(kontrol==1) 
+            break;
     }
-
-
-
-
 }
